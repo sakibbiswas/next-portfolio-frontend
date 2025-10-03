1aba5348acec7a460d094b14ae2e7b8eb19559be
Sakib Biswas <sakibsakib99880@gmail.com>
2025-10-03 19:55:54 +0600

    Notfound page  added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,3 @@
-// export default function NotFound() {
-//   return (
-//     <div className="container mx-auto py-16 text-center">
-//       <h1 className="text-5xl font-bold mb-4">404</h1>
-//       <p className="text-xl mb-4">Page not found.</p>
-//       <a href="/" className="btn">Go Home</a>
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
-
-
 
 // src/app/not-found.tsx
 import Link from "next/link";
