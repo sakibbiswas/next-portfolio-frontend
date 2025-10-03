c00fc3147bb9356b60513d8cdbc84a234872a7bd
Sakib Biswas <sakibsakib99880@gmail.com>
2025-10-03 19:51:19 +0600

    blogSlice added

--- src/lib/joinUrl.ts
+++ src/lib/joinUrl.ts
@@ -1,23 +1,3 @@
-// import { BASE_URL } from "./api";
-
-/**
- * Joins BASE_URL + relative path.
- * If the path is already absolute (http/https), returns it as is.
- */
-
-// export const joinUrl = (base: string, path?: string | null) => {
-//   if (!path) return "";
-//   if (path.startsWith("http://") || path.startsWith("https://")) {
-//     return path;
-//   }
-//   return `${base}${path}`;
-// };
-
-
-
-
-
-
 
 /**
  * Joins base URL + relative path.
